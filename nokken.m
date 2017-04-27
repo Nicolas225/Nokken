b0f559019b2b62c27e6109cebb4c7ed30ec8a6ab
Michiel Van Beirendonck <Michiel.Van.Beirendonck@gmail.com>
2017-04-27 18:16:51 +0200

    initialized files

---
+++ nokken.m
@@ -0,0 +1,11 @@
+% Gegevens :	De te ontwerpen nok moet volgende heffing kunnen realiseren :
+% 
+% van 10° tot 100° : +35 mm
+% van 180° tot 210° : -20 mm
+% van 210° tot 280° : -15 mm
+% De equivalente massa en dempingsconstante van de volger (en bijhorende onderdelen) worden respectievelijk geschat op 0.40 kg en 0.07, terwijl het mechanisme volgende statische krachten moet uitoefenen:
+% 
+% van 10° tot 190° : een konstante trek-kracht van 230 N
+% van 190° tot 210° : een konstante druk-kracht van 550 N
+% van 210° tot 250° : een lineair afnemende trek-kracht van 300 N tot 0 N
+% De gevraagde cyclustijd voor de bewerking uitgevoerd door de volger is 0.05 sec.
